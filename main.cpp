6fa0c72985b837e638082dc64607e5070b000c5b
C-W-Z <chenweizhang3021@gmail.com>
2024-01-11 20:16:45 +0800

    2020-11-07 to 2020-12-5

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    QFont font;
+    font.setFamily("Microsoft JhengHei");
+    font.setPointSize(10);
+    qApp->setFont(font);
+    w.setFixedSize(396,533);
+    w.setWindowTitle("Uncertainty Calculator v1.4");
+    w.setWindowIcon(QIcon(":/icon_256x256.ico"));
+    w.show();
+    return a.exec();
+}
+
+/* https://www.av8n.com/physics/uncertainty-calculator.html */
---
+++ mainwindow.h
@@ -0,0 +1,44 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <about.h>
+#include <instructions.h>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_EnterButton_clicked();
+
+    void on_spinBox_valueChanged(int arg1);
+
+    void on_about_triggered();
+
+    void on_instructions_triggered();
+
+    void on_Scientific_notation_stateChanged(int arg1);
+
+    void on_equalsButton_clicked();
+
+    void on_spinBox_2_valueChanged(int arg1);
+
+    void on_tabWidget_currentChanged(int index);
+
+    void on_Scientific_notation_2_stateChanged(int arg1);
+
+private:
+    Ui::MainWindow *ui;
+    About *f1;
+    Instructions *f2;
+};
+#endif // MAINWINDOW_H
